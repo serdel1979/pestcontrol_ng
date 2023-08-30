99116e13d84b8c2c42bb643d1aca8cc292de732f
sergio <sdlbsso@gmail.com>
2023-08-30 09:53:55 -0300

    nuevo formulario carga de clientes

--- src/app/clients/add-client2/add-client2.component.ts
+++ src/app/clients/add-client2/add-client2.component.ts
@@ -10,20 +10,9 @@ import { Client } from 'src/app/interfaces/client.interface';
 })
 export class AddClient2Component {
 
-  clientForm: FormGroup;
+  public clientForm!: FormGroup;
 
-  constructor(private fb: FormBuilder){
-    this.clientForm = this.fb.group({
-      businessName: ['', Validators.required],
-      cuit: ['', Validators.required],
-      contact: this.fb.group({
-        name: ['', Validators.required],
-        surname: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        phones: this.fb.array([]) 
-      }),
-    });
-  }
+  constructor(private fb: FormBuilder){}
 
   get phoneForms() {
     return this.clientForm.get('contact.phones') as FormArray;
@@ -41,26 +30,39 @@ export class AddClient2Component {
   }
 
   ngOnInit(): void {
+    this.clientForm = this.fb.group({
+      businesName: ['', Validators.required],
+      cuit: ['', Validators.required],
+      contact: this.fb.group({
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phones: this.fb.array([]) 
+      }),
+    });
     this.subscribeToInputChanges();
   }
 
 
   private subscribeToInputChanges() {
-    const inputFields = ['businesName', 'cuit', 'name', 'surname']; // Add more fields if needed
-
+    const inputFields = ['businesName', 'cuit', 'contact.name', 'contact.surname'];
+  
     inputFields.forEach(fieldName => {
       const control = this.clientForm.get(fieldName);
-
-      control?.valueChanges.pipe(
-        debounceTime(300)
-      ).subscribe(newValue => {
-        const uppercaseValue = newValue.toUpperCase();
-        if (uppercaseValue !== control.value) {
-          control.setValue(uppercaseValue, { emitEvent: false });
-        }
-      });
+  
+      if (control) {
+        control.valueChanges.pipe(
+          debounceTime(200)
+        ).subscribe(newValue => {
+          const uppercaseValue = newValue.toUpperCase();
+          if (uppercaseValue !== control.value) {
+            control.setValue(uppercaseValue, { emitEvent: false });
+          }
+        });
+      }
     });
   }
+  
 
   onSubmit() {
     if (this.clientForm.valid) {
