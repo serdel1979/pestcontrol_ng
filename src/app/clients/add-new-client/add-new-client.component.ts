b02444cf99e8eed6ffdb39896d1ee83747da75c3
sergio <sdlbsso@gmail.com>
2023-09-20 12:20:39 -0300

    carga cliente y sucursales ok

--- src/app/clients/add-new-client/add-new-client.component.ts
+++ src/app/clients/add-new-client/add-new-client.component.ts
@@ -28,14 +28,14 @@ export class AddNewClientComponent implements OnDestroy, OnInit {
   });
 
   public contactForm: FormGroup = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phones: this.fb.array([])
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phones: this.fb.array([])
   })
 
   public branchForm: FormGroup = this.fb.group({
-    clientId: [0, Validators.required], 
+    clientId: [0, Validators.required],
     name: ['', Validators.required],
     address: this.fb.group({
       street: ['', Validators.required],
@@ -48,15 +48,15 @@ export class AddNewClientComponent implements OnDestroy, OnInit {
   });
 
 
-  
-  showFormAndTable: boolean = false; 
+
+  showFormAndTable: boolean = false;
 
 
   branches: any[] = [];
 
-  displayedColumns: string[] = ['name', 'street', 'number', 'floor','zipcode','apartment','city','action'];
+  displayedColumns: string[] = ['name', 'street', 'number', 'floor', 'zipcode', 'apartment', 'city', 'action'];
   public dataSource = new MatTableDataSource<any>();
-  
+
 
 
 
@@ -74,136 +74,150 @@ export class AddNewClientComponent implements OnDestroy, OnInit {
 
 
   ngOnDestroy(): void {
-   //this.checkData();
+    //this.checkData();
   }
 
 
-    get phoneForms() {
-      return this.contactForm.get('phones') as FormArray;
+  get phoneForms() {
+    return this.contactForm.get('phones') as FormArray;
+  }
+
+
+  addPhone() {
+    const phoneFormGroup = this.fb.group({
+      number: ['', Validators.required]
+    });
+    this.phoneForms.push(phoneFormGroup);
+  }
+
+  removePhone(index: number) {
+    this.phoneForms.removeAt(index);
+  }
+
+
+
+
+  onSubmit() {
+    if (this.clientForm.valid) {
+      this.tabGroup.selectedIndex = 1;
     }
+  }
 
-
-    addPhone() {
-      const phoneFormGroup = this.fb.group({
-        number: ['', Validators.required]
-      });
-      this.phoneForms.push(phoneFormGroup);
+  submitContact() {
+    if (this.contactForm.valid) {
+      this.tabGroup.selectedIndex = 3;
     }
-  
-    removePhone(index: number) {
-      this.phoneForms.removeAt(index);
+  }
+
+  back() {
+    window.history.back();
+  }
+
+  addBranche() {
+    if (this.branchForm.valid) {
+      this.branches.push(this.branchForm.value);
+      this.dataSource.data = this.branches;
+      this.branchForm.reset();
+      this.clearFormErrors(this.branchForm);
+
     }
-  
+  }
 
-
-
-    onSubmit() {
-      if (this.clientForm.valid) {
-        this.tabGroup.selectedIndex = 1;
+  clearFormErrors(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(key => {
+      const control = formGroup.get(key);
+      if (control instanceof FormGroup) {
+        this.clearAddressErrors(control);
+      } else {
+        control?.setErrors(null);
       }
+    });
+  }
+
+  clearAddressErrors(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(key => {
+      formGroup.get(key)?.setErrors(null);
+    });
+  }
+
+
+  save() {
+    this.sending = true;
+    if (this.branchForm.valid && this.branchForm.touched) {
+      this.alertDialogService.openAlertDialog('Tiene datos sin guardar');
+      this.sending = false;
+      return;
     }
 
-    submitContact(){
-      if(this.contactForm.valid){
-        this.tabGroup.selectedIndex = 3;
-      }
+    this.saved = true;
+    const clientNew = {
+      client: this.clientForm.value,
+      branches: this.branches
     }
 
-    back() {
-      window.history.back();
-    }
+    console.log(clientNew);
+    this.clients.newClient(clientNew)
+      .subscribe(res => {
+        this.sending = false;
+        this.router.navigate(['/clients/allclients']);
+      },
+        (err) => {
+          this.sending = false;
+          this.alertDialogService.openAlertDialog('Error en la solicitud');
+        })
 
-    addBranche() {
-      if (this.branchForm.valid) {
-        this.branches.push(this.branchForm.value);
-        this.dataSource.data = this.branches;
-        this.branchForm.reset();
-        this.clearFormErrors(this.branchForm);
+  }
 
-      }
-    }
-    
-    clearFormErrors(formGroup: FormGroup) {
-      Object.keys(formGroup.controls).forEach(key => {
-        const control = formGroup.get(key);
-        if (control instanceof FormGroup) {
-          this.clearAddressErrors(control);
-        } else {
-          control?.setErrors(null);
-        }
-      });
-    }
-    
-    clearAddressErrors(formGroup: FormGroup) {
-      Object.keys(formGroup.controls).forEach(key => {
-        formGroup.get(key)?.setErrors(null);
-      });
-    }
-    
-
-    save(){
-      if(this.branchForm.valid && this.branchForm.touched){
-        this.alertDialogService.openAlertDialog('Tiene datos sin guardar');
-        return;
-      }
-
-      this.saved = true;
-      const clientNew = {
-        client: this.clientForm.value,
-        branches: this.branches
-      }
-
-      console.log(clientNew);
-
-      this.router.navigate(['/clients/allclients']);
-    }
-
-    toggleFormAndTable() {
-      this.showFormAndTable = !this.showFormAndTable;
-    }
+  toggleFormAndTable() {
+    this.showFormAndTable = !this.showFormAndTable;
+  }
 
 
-  
 
-    private subscribeToInputChanges() {
-      const inputFields = ['businessName','cuit'];
-  
-      inputFields.forEach(fieldName => {
-        const control = this.clientForm.get(fieldName);
-  
-        if (control) {
-          control.valueChanges.pipe(
-            debounceTime(200)
-          ).subscribe(newValue => {
+
+  private subscribeToInputChanges() {
+    const inputFields = ['businessName', 'cuit'];
+
+    inputFields.forEach(fieldName => {
+      const control = this.clientForm.get(fieldName);
+
+      if (control) {
+        control.valueChanges.pipe(
+          debounceTime(200)
+        ).subscribe(newValue => {
+          if (newValue) {
             const uppercaseValue = newValue.toUpperCase();
             if (uppercaseValue !== control.value) {
               control.setValue(uppercaseValue, { emitEvent: false });
             }
-          });
-        }
-      });
-    }
+          }
+        });
+      }
+    });
+  }
 
-    private subscribeToInputChangesBranche() {
-      const inputFields = ['name', 'address.street', 'address.number', 'address.floor','address.zipcode','address.apartment','address.city'];
-  
-      inputFields.forEach(fieldName => {
-        const control = this.branchForm.get(fieldName);
-  
-        if (control) {
-          control.valueChanges.pipe(
-            debounceTime(200)
-          ).subscribe(newValue => {
+  private subscribeToInputChangesBranche() {
+    const inputFields = ['name', 'address.street', 'address.number', 'address.floor', 'address.zipcode', 'address.apartment', 'address.city'];
+
+    inputFields.forEach(fieldName => {
+      const control = this.branchForm.get(fieldName);
+
+      if (control) {
+        control.valueChanges.pipe(
+          debounceTime(200)
+        ).subscribe(newValue => {
+          if (newValue) {
             const uppercaseValue = newValue.toUpperCase();
             if (uppercaseValue !== control.value) {
               control.setValue(uppercaseValue, { emitEvent: false });
             }
-          });
-        }
-      });
-    }
+          }
+        });
+      }
+    });
+  }
 
 
-    
+
 
 }
